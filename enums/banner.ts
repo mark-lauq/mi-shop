92736147de84a72f014d7ff57cf04782da531bd8
Mark LauQ <mark.lauq@gmail.com>
2025-06-11 19:45:32 +0800

    feat: define banners schema

---
+++ enums/banner.ts
@@ -0,0 +1,14 @@
+/**
+ * 广告图类型枚举
+ */
+export enum BannerType {
+  HOME_HERO = "home_hero",
+  HOME_HERO_SUB = "home_hero_sub",
+  HOME_BANNER = "home_banner",
+  HOME_BRICK = "home_brick",
+}
+
+export enum TargetType {
+  BLANK = "_blank",
+  SELF = "_self",
+}
